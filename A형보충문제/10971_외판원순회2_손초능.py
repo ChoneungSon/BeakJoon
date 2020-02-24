8ef8eb47e58256e679fbb8545609191e37c49cff
ChoneungSon <thschsmd@naver.com>
2020-02-25 01:17:24 +0900

    200225 A형 스터디 문제 풀이 코드

--- A형보충문제/10971_외판원순회2_손초능.py
+++ A형보충문제/10971_외판원순회2_손초능.py
@@ -1,11 +1,11 @@
 def dfs(sp, ep, price, visited):
     global arr, min_price, n
-    if visited[ep] == 2 and 0 not in visited:
+    if visited[ep] == 2 and 0 not in visited: # 종료 포인트의 visited가 2이고(두번들렀다는 뜻) 나머지 지점들을 모두 방문
         if price < min_price:
             min_price = price
     else:
         for i in range(n):
-            if (visited[i] == 0 or (i == ep and 0 not in visited)) and arr[sp][i]:
+            if (visited[i] == 0 or (i == ep and 0 not in visited)) and arr[sp][i]: # 한번도 방문하지 않았거나, 모든 곳을 순회한 후 되돌아 왔을 경우 dfs 진행
                 visited[i] += 1
                 dfs(i, ep, price+arr[sp][i], visited[:])
                 visited[i] -= 1
