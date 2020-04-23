afcf62ff30dc72a2b069b9fe3cd006fd9d17bbff
ChoneungSon <thschsmd@naver.com>
2020-04-23 23:01:30 +0900

    0423 알고리즘문제풀이

--- A형보충문제/불량 사용자_손초능.py
+++ A형보충문제/불량 사용자_손초능.py
@@ -1,10 +1,21 @@
 def solution(user_id, banned_id):
-    answer, adj = 0, [[0]*len(banned_id) for _ in range(len(user_id))]
+    answer, adj, visited = 0, [[0]*len(banned_id) for _ in range(len(user_id))], []
     for i in range(len(user_id)):
         for j in range(len(banned_id)):
             if find(user_id[i], banned_id[j]): adj[i][j] = 1
-    for i in range(len(user_id)):
-        print(adj[i])
+    def dfs(visit, cnt):
+        nonlocal user_id, banned_id, answer, visited
+        if cnt == len(banned_id):
+            if visit not in visited:
+                visited.append(visit[:])
+                answer += 1
+        else:
+            for i in range(len(user_id)):
+                if adj[i][cnt] and visit[i] == 0:
+                    visit[i] = 1
+                    dfs(visit[:], cnt+1)
+                    visit[i] = 0
+    dfs([0]*len(user_id), 0)
     return answer
 
 def find(str1, str2):
---
+++ A형보충문제/호텔 방 배정.py
@@ -0,0 +1,68 @@
+class node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+
+
+
+def solution(k, room_number):
+    head = node(value=room_number[0])
+    pre, fro = None, head
+    for i in range(1, len(room_number)):
+        if fro.value > room_number[i]: pre, fro = None, head
+        while fro:
+            if fro.value > room_number[i]:
+                new = node(value=room_number[i])
+                if pre: pre.next = new; new.next = fro;
+                else: head = new; new.next = fro;
+                pre = new;
+                break
+            elif fro.value == room_number[i]:
+                room_number[i] += 1
+            pre = fro; fro = fro.next
+        if fro == None:
+            pre.next = node(value=room_number[i])
+            pre, fro = None, head
+    return room_number
+
+def solution2(k, room_number):
+    rooms = dict()
+    for i in range(len(room_number)):
+        num = room_number[i]
+        while 1:
+            if num not in rooms:
+                rooms[num] = num + 1
+                room_number[i] = num
+                break
+            temp = num
+            num = rooms[num]
+            if rooms.get(num): rooms[temp] = rooms[num] + 1
+    return room_number
+
+
+import sys
+
+sys.setrecursionlimit(10000000)  # 설정해주지 않으면 재귀가 많이 일어나면서 런타임에러 등이 나타날 수 있다.
+
+
+def findEmptyRoom(number, rooms):  # 빈방을 찾는 함수
+    if number not in rooms:
+        rooms[number] = number + 1
+        return number
+
+    empty = findEmptyRoom(rooms[number], rooms)
+    rooms[number] = empty + 1
+    return empty
+
+
+def solution3(k, room_number):
+    answer = []
+    rooms = dict()  # 몇번 방이 비어있는지 체크하는 딕셔너리
+
+    for number in room_number:
+        emptyRoom = findEmptyRoom(number, rooms)
+        answer.append(emptyRoom)
+
+    return answer
+
+print(solution2(10, [1]*200000))
